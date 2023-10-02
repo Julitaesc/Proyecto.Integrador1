72a6cf9f0f63f4a05cbcb0f4de2b6f522e600d40
Juliana Escudero <julianaaescudero@gmail.com>
2023-10-02 16:06:50 -0300

    Subida a Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
+import { resolve } from "path"
+
+// console.log(resolve("pages")) //
+
 export default {
     server: {
         port: "2222",
     },
     css: {
         devSourcemap: true,
-    }
+},
+
+build: {
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+            carrito: resolve("pages/carrito.html"),
+            contacto: resolve("pages/contacto.html"),
+            nosotros: resolve("pages/nosotros.html"),
+            inicio: resolve("index.html")
 }
+}
+}
+};
